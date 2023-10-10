5396b0765d6bb09edec22d4e17cb2f9fe7df47a8
Henry Elano Klitzke <henry.elano@gmail.com>
2023-10-10 20:33:07 -0300

    Aula 1 e 2 Curso Java Spring

---
+++ src/main/java/br/com/henryelano/todolist/user/UserController.java
@@ -0,0 +1,34 @@
+package br.com.henryelano.todolist.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    
+    @Autowired
+    private IUserRepository userRepository;
+
+    @PostMapping("/")
+    public ResponseEntity create(@RequestBody UserModel oUser) {
+        if(this.userExist(oUser)) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário Já existe!");
+        }
+        return ResponseEntity.ok().body(this.userRepository.save(oUser));
+    } 
+
+    /**
+     * Retorna se o usuário já está cadastrado.
+     * @param UserModel oUser
+     * @return boolean
+     */
+    private boolean userExist(UserModel oUser) {
+        return this.userRepository.findByUsername(oUser.getUsername()) != null ? true : false;
+    }
+}
